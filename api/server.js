9e3060c7d29cd316c48e86b406c97bc0819f07a3
lgallo95 <lgallo2005@gmail.com>
2022-01-10 20:43:51 -0800

    done

--- api/server.js
+++ api/server.js
@@ -81,4 +81,22 @@ server.delete("/api/users/:id", async (req, res) => {
   }
 });
 
+server.put('/api/users/:id', async (req, res) => {
+    const { id } = req.params
+    const { name, bio } = req.body
+    const updateUser = await Users.update(id, { name, bio })
+    
+    try {
+        if (!updateUser) {
+            res.status(404).json({ message: "The user with the specified ID does not exist" })
+        } else if (!name || !bio) {
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        } else {
+            res.status(200).json(updateUser)
+        }
+    } catch (err) {
+        res.status(500).json({ message: "The user information could not be modified" })
+    }
+})
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
